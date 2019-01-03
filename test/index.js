ebfba38cd46bc40cf29aa20d8fe33168c64fb3a7
Fábio Santos <fabiosantosart@gmail.com>
2019-01-03 14:28:44 +0000

    can use function and class components

--- test/index.js
+++ test/index.js
@@ -13,6 +13,67 @@ describe('react-applications', () => {
         key: null,
         props: {
           foo: 'bar'
+        },
+        children: []
+      }
+    )
+  })
+  it('can invoke function and class components', () => {
+    function FComp(props) { return props.foo }
+    class CComp extends React.Component {
+      render() { return this.props.bar }
+    }
+    assert.deepEqual(
+      reactApp(
+        <div>
+          <FComp foo="bar" />
+          <CComp bar="baz" />
+        </div>
+      ),
+      {
+        type: 'div',
+        key: null,
+        props: {},
+        children: [
+          {
+            type: FComp,
+            key: null,
+            props: {
+              foo: 'bar',
+            },
+            children: []
+          }, {
+            type: CComp,
+            key: null,
+            props: {
+              bar: 'baz'
+            },
+            children: []
+          }
+        ]
+      }
+    )
+  })
+  it.skip('reads JSX into an object', () => {
+    assert.deepEqual(
+      reactApp(<div foo="bar" />),
+      {
+        type: 'div',
+        key: null,
+        props: {
+          foo: 'bar'
+        }
+      }
+    )
+  })
+  it.skip('reads JSX into an object', () => {
+    assert.deepEqual(
+      reactApp(<div foo="bar" />),
+      {
+        type: 'div',
+        key: null,
+        props: {
+          foo: 'bar'
         }
       }
     )
